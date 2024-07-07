df1750d077ead7d6e8827d7054a7ea7df821d60e
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2024-07-07 05:43:31 +0100

    Dev initial pull (#1)
    * imported tinyfmt
    
    * added basic log functionality
    
    * Create LICENSE
    
    * Create README.md
    
    * imported tinyfmt
    
    * corrected function call

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Jason-Duffy/tinylog
+
+go 1.21.4
+
+require github.com/Jason-Duffy/tinyfmt v0.1.2
+
+require github.com/Jason-Duffy/tinystrconv v0.1.1 // indirect
---
+++ log.go
@@ -0,0 +1,22 @@
+package tinylog
+
+import (
+	"os"
+
+	"github.com/Jason-Duffy/tinyfmt"
+)
+
+// Logger is a simple logger struct
+type Logger struct {
+	output *os.File
+}
+
+// NewLogger creates a new Logger instance
+func NewLogger(output *os.File) *Logger {
+	return &Logger{output: output}
+}
+
+// Log logs a formatted message
+func (l *Logger) Log(format string, args ...interface{}) error {
+	return tinyfmt.PrintToIo(l.output, format, args...)
+}
